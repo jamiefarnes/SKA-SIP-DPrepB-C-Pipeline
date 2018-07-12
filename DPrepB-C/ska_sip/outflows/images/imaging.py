a3fae97e933231abec55a5b12f45c9e8acfa9cb5
Jamie Farnes <jamie.farnes@oerc.ox.ac.uk>
2018-07-12 09:42:12 +0100

    remove DPrepA

--- DPrepB-C/ska_sip/outflows/images/imaging.py
+++ DPrepB-C/ska_sip/outflows/images/imaging.py
@@ -41,9 +41,9 @@ def wstack(vis, npixel_advice, cell_advice, channel, results_dir):
         vis, _, _ = weight_visibility(vis, model)
         
         # Create a dirty image:
-        dirty, sumwt = invert_function(vis, model, context='2d', vis_slices=1, padding=2)
+        dirty, sumwt = invert_function(vis, model, context='wstack', facets=1, vis_slices=42)
         # Create the psf:
-        psf, sumwt = invert_function(vis, model, dopsf=True, context='2d', vis_slices=1, padding=2)
+        psf, sumwt = invert_function(vis, model, dopsf=True, context='wstack', facets=1, vis_slices=42)
         
         # Save to disk:
         export_image_to_fits(dirty, '%s/imaging_dirty_WStack-%s.fits'
@@ -90,50 +90,6 @@ def image_2d(vis, npixel_advice, cell_advice, channel, results_dir):
                              % (results_dir, channel))
         export_image_to_fits(psf, '%s/imaging_psf_WStack-%s.fits'
                              % (results_dir, channel))
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
-    return dirty, psf
-
-
-def image_mfs(vis, npixel_advice, cell_advice, channel, results_dir):
-    """Do mfs imaging of visibility data.
-        
-        Args:
-        vis (obj): ARL visibility data.
-        npixel_advice (float): number of pixels in output image.
-        cell_advice (float): cellsize in output image.
-        channel (int): channel number to be imaged (affects output filename).
-        results_dir (str): directory to save results.
-        
-        Returns:
-        dirty: dirty image.
-        psf: image of psf.
-        """
-    try:
-        vis_slices = len(np.unique(vis.time))
-        print("There are %d timeslices" % vis_slices)
-        # Obtain advice on w-proj parameters:
-        advice = advise_wide_field(vis)
-        
-        print(vis.frequency)
-        centre_frequency = np.array([np.average(vis.frequency)])
-        
-        # Create a model image:
-        model = create_image_from_visibility(vis, cellsize=cell_advice, npixel=npixel_advice, phasecentre=vis.phasecentre, frequency=centre_frequency, polarisation_frame=PolarisationFrame('stokesIQUV'))
-      
-        # Weight the visibilities:
-        vis, _, _ = weight_visibility(vis, model)
-        
-        dirty, sumwt = invert_function(vis, model, context='2d', padding=1)
-
-        psf, sumwt = invert_function(vis, model, context='2d', dopsf=True, padding=1)
-        
-        # Save to disk:
-        export_image_to_fits(dirty, '%s/imaging_dirty_mfs-%s.fits'
-                             % (results_dir, channel))
-        export_image_to_fits(psf, '%s/imaging_psf_mfs-%s.fits'
-                            % (results_dir, channel))
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
